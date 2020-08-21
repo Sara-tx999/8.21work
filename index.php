9e1cfd618ae23ee8f57719e6642250d7e5b4c1c3
Sara-tx999 <1755945317@qq.com>
2020-08-21 11:14:16 +0800

    first commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+
+define("MVC","true");
+define("APP_DIR_NAME","application");
+define("APP_NAME",__DIR__.DIRECTORY_SEPARATOR.APP_DIR_NAME.DIRECTORY_SEPARATOR);
+
+require_once "libs/start.php";
+
+
+
+
+
